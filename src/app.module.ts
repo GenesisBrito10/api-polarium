c2d50f53a28df5505856dd713bbd13b1b3d53666
Gênesis Brito <76199494+GenesisBrito10@users.noreply.github.com>
2025-06-06 17:41:44 -0300

    feat: add webhook and mongo order history

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module, Logger } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js'; // Import AppService
 import { SdkModule } from './sdk/sdk.module.js';
 import { TradingModule } from './trading/trading.module.js';
 import { AccountModule } from './account/account.module.js';
 import { OrderModule } from './order/order.module.js';
+import { WebhookModule } from './webhook/webhook.module.js';
+import { WebsocketModule } from './websocket/websocket.module.js';
 import configuration from './config/configuration.js'; // Adjust the path as necessary
 
 @Module({
@@ -14,10 +17,13 @@ import configuration from './config/configuration.js'; // Adjust the path as nec
       isGlobal: true, // Makes ConfigModule available globally
       load: [configuration], // Load custom configuration
     }),
+    MongooseModule.forRoot('mongodb://automagroup:3nWLOXv168Bg@177.93.108.140:27018/?authSource=admin'),
     SdkModule,
     TradingModule,
     AccountModule,
     OrderModule,
+    WebhookModule,
+    WebsocketModule,
     
   ],
   controllers: [AppController],
